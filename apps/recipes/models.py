f9dbaf611880c1a8898ffbe58bd9bde1c6cdf0ec
disreal48 <disreal48@googlemail.com>
2024-08-08 19:53:38 +0200

    adjusted difficulty to be a property

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -8,20 +8,18 @@ class Recipe(models.Model):
         max_length=255, help_text="Enter ingredients comma separated"
     )
     cooking_time = models.IntegerField(help_text="Enter cooking time (minutes)")
-    difficulty = models.CharField(
-        max_length=20, editable=False, default="easy"
-    )
-    
-    def calculate_difficulty(self):
+
+    @property
+    def difficulty(self):
       ingredients_len = len(self.return_ingredients_as_list())
       if self.cooking_time < 10 and ingredients_len < 4:
-        self.difficulty =  "Easy"
+        return "Easy"
       elif self.cooking_time < 10 and ingredients_len >= 4:
-        self.difficulty =  "Medium"
+        return "Medium"
       elif self.cooking_time >= 10 and ingredients_len < 4:
-        self.difficulty =  "Intermediate"
+        return "Intermediate"
       else:
-        self.difficulty =  "Hard"
+        return "Hard"
 
     def __str__(self):
         return f"{self.name} - {self.difficulty} - {self.cooking_time}"
