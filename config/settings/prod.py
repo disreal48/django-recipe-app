49adaacb500d705339494c39c8dd2e29705911a7
disreal48 <disreal48@googlemail.com>
2024-09-12 14:40:29 +0200

    set up cloudinary support

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,6 +2,17 @@ import environ
 import os
 import dj_database_url
 from .base import *
+import cloudinary
+import cloudinary.uploader
+from cloudinary.utils import cloudinary_url
+
+# Configuration       
+cloudinary.config( 
+    cloud_name = env.str("CLOUD_NAME"), 
+    api_key = env.str("API_KEY"), 
+    api_secret = env.str("api_secret"),
+    secure=True
+)
 
 env = environ.Env(DEBUG=(bool, True))
 environ.Env.read_env(str(BASE_DIR / ".env_prod"))
@@ -10,9 +21,19 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG")
 
 MIDDLEWARE = ["whitenoise.middleware.WhiteNoiseMiddleware"] + MIDDLEWARE
+INSTALLED_APPS = [
+  'cloudinary_storage',
+  'django.contrib.staticfiles',
+  'cloudinary',
+] + INSTALLED_APPS 
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env.str("CLOUD_NAME"),
+    'API_KEY': env.str("API_KEY"),
+    'API_SECRET': env.str("api_secret")
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 
