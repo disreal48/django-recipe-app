699ef72f0cfa35187845ea1e651eb9144fdd68c0
disreal48 <disreal48@googlemail.com>
2024-09-12 13:10:00 +0200

    styled error page and added prod infasctucture

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf.urls import handler404
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def custom_page_not_found_view(request, exception):
     return redirect(f"{reverse('user:login_view')}?next={request.path}")
@@ -12,5 +14,8 @@ urlpatterns = [
     path('', include('apps.recipes.urls', namespace='recipes')),
     path('user/', include('apps.user.urls', namespace='user')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = custom_page_not_found_view
