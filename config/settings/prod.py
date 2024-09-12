19903f610d85182be6a2b0693817db4ac9175679
disreal48 <disreal48@googlemail.com>
2024-09-12 15:31:23 +0200

    updated requirements

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -20,12 +20,23 @@ environ.Env.read_env(str(BASE_DIR / ".env_prod"))
 SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG")
 
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1", "0.0.0.0"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://localhost",
+    "https://127.0.0.1",
+    "https://0.0.0.0",
+    "https://mysterious-brushlands-76647.herokuapp.com",
+]
+
 MIDDLEWARE = ["whitenoise.middleware.WhiteNoiseMiddleware"] + MIDDLEWARE
 INSTALLED_APPS = [
   'cloudinary_storage',
   'django.contrib.staticfiles',
   'cloudinary',
 ] + INSTALLED_APPS 
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 CLOUDINARY_STORAGE = {
@@ -35,21 +46,9 @@ CLOUDINARY_STORAGE = {
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
